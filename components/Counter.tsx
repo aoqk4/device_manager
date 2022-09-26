682e19cc90c53720faf0dcdd80ed97cbe4ed3340
lsj <qazplm807733@gmail.com>
2022-09-26 13:18:50 +0900

    add readme

---
+++ components/Counter.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+interface CounterProps {
+  title: String | Number | Number[];
+}
+
+const Counter = (props: CounterProps) => {
+  const { title } = props;
+  const [cnt, setCnt] = useState(0);
+  return (
+    <div>
+      <h2>{title.toString()}</h2>
+      <h2>카운터 {cnt}</h2>
+      <button
+        onClick={() => {
+          setCnt(cnt + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCnt(cnt - 1);
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import Counter from "../components/Counter";
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Counter title={[1, 2, 3, 4, 5, parseInt("123")]} />
+    </div>
+  );
 };
 
 export default Home;
